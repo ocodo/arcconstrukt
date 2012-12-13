ef61bed764fcff4e27f1913cad323c5174606c1c
Jasonm23 <jasonm23@gmail.com>
2012-12-14 01:26:24 +1100

    added auto toc generation and jump menu to tutorial

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,8 +3,21 @@
 if [ -e tutorial.markdown ] && [ -e t.head ] && [ -e t.foot ]; then
 #    gem install redcarpet
     redcarpet --render-with_toc_data tutorial.markdown > tmp
-    cat t.head tmp t.foot > ../../webroot/tutorial.html
-    rm tmp
+
+    grep toc tmp | 
+    tr '<>/="' ' ' | 
+    sed -e 's/h[21]//g' -e 's/ id //g' -e 's/.*toc/toc/' | 
+    awk '{ 
+          link=$1; 
+          $1=""; 
+          title=$0; 
+          printf "<option value=\"#%s\">%s</option>\n", link, title 
+    }' |
+    sed '$d' > toc
+
+    cat t.head toc t.select-close tmp t.foot > ../../webroot/tutorial.html
+    rm tmp toc
+
     open -a Chromium ../../webroot/tutorial.html
 else
     echo "Run this from the tutorial folder" 
