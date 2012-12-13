d80b4d2aaf2846fadf97ba7627fc7a55e1403bef
Jasonm23 <jasonm23@gmail.com>
2012-12-13 23:41:15 +1100

    update tutorial

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,17 +1,12 @@
-#!/bin/zsh --login
-
+#!/bin/bash
 
 if [ -e tutorial.markdown ] && [ -e t.head ] && [ -e t.foot ]; then
 #    gem install redcarpet
-    redcarpet --render-with_toc_data tutorial.markdown > t.html
-    cat t.head t.html t.foot > tutorial.html
-    open -a Chromium tutorial.html    
+    redcarpet --render-with_toc_data tutorial.markdown > tmp
+    cat t.head tmp t.foot > ../../webroot/tutorial.html
+    rm tmp
+    open -a Chromium ../../webroot/tutorial.html
 else
     echo "Run this from the tutorial folder" 
 fi
-
-
-
-
-
 
